8b6716a6c4b6aadb22af24cc431a071545f63d96
AbdulQaseem <abdulqaseem560@gmail.com>
2024-09-19 19:50:38 -0700

    thi is my new code

--- main.js
+++ main.js
@@ -2,27 +2,34 @@ const demo_div = document.getElementById('Demo');
 const input = document.getElementById('User');
 const btn = document.getElementById('btn')
 
+const api_key ="105b3449e54997efc78dbad890bf50dc";
+
 const popularMovies = async () => {
 
-        let api = await fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=105b3449e54997efc78dbad890bf50dc`);
+        let api = await fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}`);
 
         let data = await api.json();
 
         console.log(data);
         
         demo_div.innerHTML = data.results.map((item) => 
-        
-            `<div class="posters">
-                <img src="https://image.tmdb.org/t/p/w1280${item.poster_path}">
-                <h3>${item.title}</h3>
-                <div class="details"> 
-                <p><b> Views : ${item.vote_average}K</b></p>
-                 <p><b> Date : ${item.release_date}</b></p>
-                </div>
-            </div>`
+        `<div class="posters">
+        <img src="https://image.tmdb.org/t/p/w1280${item.poster_path}"/>
+                <div class="overlay">
+                    <div class="title"> 
+                        <h3> ${item.title} </h3>
+                        <span> ${item.vote_average} <span>
+                    </div>
+                    <br>
+                    <h3>Overview:</h3>
+                    <p> 
+                        ${item.overview}
+                    </p>
+                    <button class="trailerBtn">Watch Trailer</button>
+                 </div>
+                 </div>`
 
-        
-) 
+        ).join(''); 
 };
 
 popularMovies();
@@ -31,23 +38,31 @@ const SearchMovies = async () => {
     
     demo_div.innerHTML = ""
 
-    let searchApi = await fetch(`https://api.themoviedb.org/3/search/movie?&api_key=105b3449e54997efc78dbad890bf50dc&query=${input.value}`);
+    let searchApi = await fetch(`https://api.themoviedb.org/3/search/movie?&api_key=${api_key}&query=${input.value}`);
 
     let data = await searchApi.json();
 
    
     demo_div.innerHTML = data.results.map((item)=>
         
-        `
-        <div class="main">
-                <img src="https://image.tmdb.org/t/p/w1280${item.poster_path}">
-                <h3>${item.title}</h3>
-                <div class="details"> 
-                <p><b> Views : ${item.vote_average}K</b></p>
-                 <p><b> Date : ${item.release_date}</b></p>
-                </div>
-            </div>
-        `
-)
+        `<div class="main">
+        <img src="https://image.tmdb.org/t/p/w1280${item.poster_path}"/>
+                <div class="overlay">
+                    <div class="title"> 
+                        <h3> ${item.title}  </h3>
+                        <span> ${item.vote_average} <span>
+                    </div>
+                    <br>
+                    <h3>Overview:</h3>
+                    <p> 
+                        ${item.overview}
+                    </p>
+                    <button class="trailerBtn">Watch Trailer</button>
+                 </div>
+                 </div>`
+                
+                ).join('');
         
 };
+
+
